eb739bf72fa60d14496102fd2e74653757f7b667
Wasim Akram <mr.saim516@gmail.com>
2025-04-18 22:42:08 +0500

    update

--- src/app/cms/businesses/[businessSlug]/games/[gameSlug]/page.js
+++ src/app/cms/businesses/[businessSlug]/games/[gameSlug]/page.js
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 
 export default function GameSlugRedirect({ params }) {
   const { gameSlug } = params;
+  const { businessSlug } = params;
 
-  redirect(`/cms/businesses/${gameSlug}/games`);
+  redirect(`/cms/businesses/${businessSlug}/games/${gameSlug}`);
 }
